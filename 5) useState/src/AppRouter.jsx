f96db6b26d5bccc5fe463b6b80dc542dfcb85681
JonaTheBridge <jonathan@thebridgeschool.es>
2023-06-30 19:20:10 +0200

    clear useState class

--- 5) useState/src/AppRouter.jsx
+++ 5) useState/src/AppRouter.jsx
@@ -1,9 +1,4 @@
 import { useRoutes } from 'react-router-dom';
-import App from './pages/App/App.jsx';
-import Flags from './pages/Flags/Flags.jsx';
-import URLParams from './pages/URLParams/URLParams.jsx';
-import Bookings from './pages/Bookings/Bookings.jsx';
-import BookingDetail from './pages/BookingDetail/BookingDetail.jsx';
 import UseState from './pages/UseState/UseState.jsx';
 import Calc from './pages/Calc/Calc.jsx';
 
@@ -11,24 +6,8 @@ function AppRouter() {
   return useRoutes(
     [
       {
-        element: <App />,
+        element: <UseState />,
         path: '/',
-      },
-      {
-        element: <Flags />,
-        path: '/flags',
-      },
-      {
-        element: <URLParams />,
-        path: '/url-params/:param',
-      },
-      {
-        element: <Bookings />,
-        path: '/bookings',
-      },
-      {
-        element: <BookingDetail />,
-        path: '/bookingDetail/:id',
       },
       {
         element: <UseState />,
